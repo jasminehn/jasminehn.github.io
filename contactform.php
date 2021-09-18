c8e1a977204b76b928a0a70b544d275072950e23
SentientBagel <jhnelson.contact@gmail.com>
2021-09-18 11:04:33 -0400

    added backend for contact form

--- contactform.php
+++ contactform.php
@@ -1,17 +1,34 @@
 <?php
 
 //if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
-    $message = $_POST['message'];
+    // $name = $_POST['name'];
+    // $subject = $_POST['subject'];
+    // $mailFrom = $_POST['mail'];
+    // $message = $_POST['message'];
 
-    $mailTo = "jhnelson.contact@gmail.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an email from ".$name.".\n\n".$message;
+    // $mailTo = "jhnelson.contact@gmail.com";
+    // $headers = "From: ".$mailFrom;
+    // $txt = "You have received an email from ".$name.".\n\n".$message;
 
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location:index.html");
+    // mail($mailTo, $subject, $txt, $headers);
+    // header("Location:index.html");
 //}
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $from = 'From: portfolio site'; 
+    $to = 'jhnelson.contact@gmail.com';
+
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+    if ($_POST['submit']) {
+        if (mail ($to, $subject, $body, $from)) { 
+           $success = "Message successfully sent";
+        } else {
+            $success = "Message Sending Failed, try again";
+        }
+    }
 
 ?>
--- script.js
+++ script.js
@@ -119,6 +119,21 @@ var typed = new Typed(".typing-text", {
 });
 
 
+//-------------------- EMAIL CONTACT FORM --------------------
+// function sendEnquiryform(){
+//     var name=$('#name').val();
+//     var email=$('#email').val();
+//     var message=$('#message').val();
+//     $.post("contactform.php",'name='+name+'&email='+email+'&message='+message,function(result,status,xhr) {
+//             if( status.toLowerCase()=="error".toLowerCase() )
+//             { alert("An Error Occurred.."); }
+//             else { 
+//                 //alert(result);
+//                 $('#sucessMessage').html(result);
+//             }
+//         })
+//         .fail(function(){ alert("something went wrong. Please try again") });
+// }
 
 
 // const $parallaxB1 = $('.home');
